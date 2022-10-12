09be4784f4a136fea4f98dfed1a3d6c9ec5927cc
youngtitanium <youngtitanium.l@gmail.com>
2022-10-12 11:09:59 +0300

    Add long_description

--- setup.py
+++ setup.py
@@ -26,5 +26,6 @@ setup(
     license='MIT',
     author='YoungTitanium',
     author_email='youngtitanium.l@gmail.com',
-    description='The simple python addon system, which allow you to load, reload, enable, disable, requirements check and install of addons'
+    description="Easier way to manage your project addons",
+    long_description='The simple python addon system, which allow you to load, reload, enable, disable, requirements check and install of addons',
 )
